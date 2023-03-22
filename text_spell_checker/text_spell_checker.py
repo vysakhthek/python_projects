4dd123f73cd0e8f93853f0bec35163037f7528e7
Vysakh Thekkath <116060493+vysakhthek@users.noreply.github.com>
2023-03-22 10:04:23 +0530

    Add files via upload
    Text spell checker python file added

---
+++ text_spell_checker/text_spell_checker.py
@@ -0,0 +1,33 @@
+from textblob import TextBlob
+
+
+tries = 1
+while tries:
+    # collecting incorrect spelling text from user
+    check_text = input("\nEnter the word to be checked:- ") 
+    
+    #printing original text
+    print("original text: "+str(check_text)) 
+    
+    #correcting the text
+    corrected_text = TextBlob(check_text) 
+    
+    # prints the corrected spelling
+    print("corrected text: "+str(corrected_text.correct()))
+    print("\nHope this helped you")
+    while True:
+        loop = (input("Do you want to Try Again?: \n1.yes \n2.no \n"))
+        if loop == '2':
+            tries = 0
+            break
+        elif loop == '1':
+            break
+        else:
+            print("\nInvalid input. Please enter 1 or 2.")
+else:
+    print("\nThank you for using Spell Checker")
+        
+
+
+
+
